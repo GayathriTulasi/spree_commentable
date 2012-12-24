6e987fea012ac0362ef1c7a04ab9e42fa2948a4e
Volodya Protasevich <ggg1321@gmail.com>
2012-12-24 17:15:01 +0400

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Spree::Core::Engine.routes.draw do
-  scope(:module => "commentable") do
 
-    resources :products do
+  resources :products do
+    scope(:module => "commentable") do
       resources :comments
     end
+  end
 
-    namespace :admin do
+  namespace :admin do
+    scope(:module => "commentable") do
       resources :comments
     end
-
-
   end
 end
