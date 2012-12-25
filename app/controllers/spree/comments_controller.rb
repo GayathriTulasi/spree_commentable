b877b5e279cdc44caae055a45e4315b9e0d2cf79
Marat Abdulin <esend.work@gmail.com>
2012-12-25 18:44:52 +0400

    fix bug product or blog comment

--- app/controllers/spree/comments_controller.rb
+++ app/controllers/spree/comments_controller.rb
@@ -1,16 +1,16 @@
 module Spree
   class CommentsController < Spree::BaseController
     def create
-      if params.include?(:$product_id)
-        @resource = Spree::Product.find_by_permalink(params[:product_id])
-      else
+      if params[:id]
         @resource = Spree::Post.find_by_path(params[:id])
+      else
+        @resource = Spree::Product.find_by_permalink(params[:product_id])
       end
       @comment = @resource.comments.create(params[:comment])
-      if params.include?(:$product_id)
-        redirect_to "/products/#{@resource.permalink}", :notice => t(:comment_added_notice)
-      else
+      if params[:id]
         redirect_to "/#{@resource.blog.permalink}/#{@resource.path}", :notice => t(:comment_added_notice)
+      else
+        redirect_to "/products/#{@resource.permalink}", :notice => t(:comment_added_notice)
       end
     end
   end
