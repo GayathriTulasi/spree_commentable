6683559b8a738d561fa3f232e26bb0a548d5a4d3
Kirill Zonov <graffzon@gmail.com>
2012-12-25 14:21:45 +0400

    fixed stack overflow error deface

--- app/overrides/product_show.rb
+++ app/overrides/product_show.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/products/show",
                      :replace => "[data-hook='product_show']",
-                     :template => "spree/products/show",
+                     :template => "spree/products/commentable_show",
                      :name => "product_show")
