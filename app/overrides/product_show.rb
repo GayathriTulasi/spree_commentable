28a77fb3097b5c7555199150811158298a267102
Kirill Zonov <graffzon@gmail.com>
2012-12-25 13:50:31 +0400

    moved views outside commentable module and changed version

--- app/overrides/product_show.rb
+++ app/overrides/product_show.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/products/show",
                      :replace => "[data-hook='product_show']",
-                     :template => "spree/commentable/products/show",
+                     :template => "spree/products/show",
                      :name => "product_show")
--- app/overrides/spree_products_comment.rb
+++ app/overrides/spree_products_comment.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path  => "spree/layouts/admin",
                      :name          => "spree_products_comment",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :partial       => "spree/commentable/admin/shared/contents_tab",
+                     :partial       => "spree/admin/shared/contents_tab",
                      :disabled      => false)
