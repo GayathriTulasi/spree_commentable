da8af14574ada668891233149818ee208a3c8f2e
Marat Abdulin <esend.work@gmail.com>
2013-01-11 18:56:41 +0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Spree::Core::Engine.routes.prepend do
   namespace :admin do
     resources :comments
     get "comments/approve/:id/:flag" => "comments#approve"
+    get "comments/full/:num" => "comments#index"
   end
 
   post ":blog_id/:id/comments" => "comments#create",  :as => :post_comments
