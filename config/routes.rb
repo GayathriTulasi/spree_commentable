8cef17bf371ebad09190f39101ba3f60db6b32a6
Volodya Protasevich <ggg1321@gmail.com>
2013-01-14 20:43:36 +0400

    Add form and routes for filter

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Spree::Core::Engine.routes.prepend do
   end
 
   namespace :admin do
-    resources :comments
-    get "comments/approve/:id/:flag" => "comments#approve"
+    resources :comments do
+      collection do
+        get  :resource_filter
+      end
+      get "comments/approve/:id/:flag" => "comments#approve"
+    end
   end
 
   post ":blog_id/:id/comments" => "comments#create",  :as => :post_comments
