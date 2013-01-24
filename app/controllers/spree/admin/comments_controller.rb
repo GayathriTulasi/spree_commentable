88807f12c40fafc2a183c6b184ebb6abd80b32de
Marat Abdulin <esend.work@gmail.com>
2013-01-24 13:03:59 +0400

    remove post resource

--- app/controllers/spree/admin/comments_controller.rb
+++ app/controllers/spree/admin/comments_controller.rb
@@ -60,31 +60,6 @@ module Spree
           end
         end
 
-    #   def new
-    #     @post = Spree::Post.new
-    #     @post.posted_at ||= Time.now
-    #   end
-
-    # private
-
-
-
-    #   def location_after_save
-    #     path = params[:redirect_to].to_s.strip.sub(/^\/+/, "/")
-    #     path.blank? ? object_url : path
-    #   end
-
-    #   def find_resource
-    #     @object ||= Spree::Post.find_by_path(params[:id])
-    #   end
-
-    #   def collection
-    #     params[:search] ||= {}
-    #     params[:search][:meta_sort] ||= "posted_at.desc"
-    #     @search = Spree::Post.search(params[:q])
-    #     @collection = @search.result.page(params[:page]).per(Spree::Config[:orders_per_page])
-    #   end
-
     end
   end
 end
--- app/controllers/spree/comments_controller.rb
+++ app/controllers/spree/comments_controller.rb
@@ -1,17 +1,9 @@
 module Spree
   class CommentsController < Spree::BaseController
     def create
-      if params[:id]
-        @resource = Spree::Post.find_by_path(params[:id])
-      else
-        @resource = Spree::Product.find_by_permalink(params[:product_id])
-      end
+      @resource = Spree::Product.find_by_permalink(params[:product_id])
       @comment = @resource.comments.create(params[:comment])
-      if params[:id]
-        redirect_to "/#{@resource.blog.permalink}/#{@resource.path}", :notice => t(:comment_added_notice)
-      else
-        redirect_to "/products/#{@resource.permalink}", :notice => t(:comment_added_notice)
-      end
+      redirect_to "/products/#{@resource.permalink}", :notice => t(:comment_added_notice)
     end
   end
 end
