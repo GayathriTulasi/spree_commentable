904e84bd8961d982bccf2f7b696c7ea49c482e8a
Marat Abdulin <esend.work@gmail.com>
2013-02-14 19:33:25 +0400

    fix comment creating, add error message

--- app/controllers/spree/commentable/comments_controller.rb
+++ app/controllers/spree/commentable/comments_controller.rb
@@ -1,7 +1,11 @@
 class Spree::Commentable::CommentsController < Spree::BaseController
   def create
     @product = Spree::Product.find_by_permalink!(params[:product_id])
-    @comment = @product.comments.create(params[:commentable_comment])
-    redirect_to product_url(@product), notice: t(:comment_added_notice)
+    @comment = @product.comments.new(params[:commentable_comment])
+    if @comment.save
+      redirect_to product_url(@product), notice: t(:comment_added_notice)
+    else
+      redirect_to product_url(@product), flash: { error: t(:comment_not_added) }
+    end
   end
 end
