81cab82f32659f0a8c165f721d58ce2318848932
Marat Abdulin <esend.work@gmail.com>
2013-02-14 15:11:11 +0400

    add to markup showing full/short comment

--- app/assets/javascripts/admin/spree_commentable.js
+++ app/assets/javascripts/admin/spree_commentable.js
@@ -1,1 +1,5 @@
-//= require admin/spree_core
+(function ($) {
+  $('.comment_line').live("click", function(){
+    $(this).parent('.comment').children('span.comment_line').toggle();
+  });
+})(jQuery);
